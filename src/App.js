aefc04ef3ee0974d0397923330f676e0bde9e7a8
Scott Cormier <chicormier@gmail.com>
2023-11-16 19:30:59 -0600

    prepared for workshop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 
 import './App.css';
 
 function App() {
-  return <div className="App">hello nucamp!</div>;
+  return (
+    <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
+    </div>
+  );
 }
 
 export default App;
