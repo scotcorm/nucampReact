509746435c3311e8aefbb22bb33fdb714432a466
Scott Cormier <chicormier@gmail.com>
2023-11-21 18:37:59 -0600

    added Detail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CampsitesList />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesDirectoryPage />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
