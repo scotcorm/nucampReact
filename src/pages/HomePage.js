637d8be7f819856c5a61ad9ade7edf95542500ec
Scott Cormier <chicormier@gmail.com>
2023-11-22 19:00:04 -0600

    added links and home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import { Container } from 'reactstrap';
+import DisplayList from '../features/display/DisplayList';
+
+const HomePage = () => {
+    return (
+        <Container>
+            <DisplayList />
+        </Container>
+    );
+};
+
+export default HomePage;
