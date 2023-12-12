d6598fe7e3d3ad79d5100cf1ed4fdd2e27efdb81
Scott Cormier <chicormier@gmail.com>
2023-12-11 19:01:35 -0600

    Fetch and createAsyncThunk

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -6,26 +8,29 @@ import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
-
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-                <Route path='about' element={<AboutPage />} />
-                <Route
-                    path='directory/:campsiteId'
-                    element={<CampsiteDetailPage />}
-                />
-            </Routes>
-            <Footer />
-        </div>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -5,19 +5,35 @@ import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommentsList';
 import SubHeader from '../components/SubHeader';
+import Error from '../components/Error';
+import Loading from '../components/Loading';
 
 const CampsiteDetailPage = () => {
   const { campsiteId } = useParams();
   const campsite = useSelector(selectCampsiteById(campsiteId));
   console.log('campsite:', campsite);
 
-  return (
-    <Container>
-      <SubHeader current={campsite.name} detail={true} />
-      <Row>
+  const isLoading = useSelector((state) => state.campsites.isLoading);
+  const errMsg = useSelector((state) => state.campsites.errMsg);
+  let content = null;
+
+  if (isLoading) {
+    content = <Loading />;
+  } else if (errMsg) {
+    content = <Error errMsg={errMsg} />;
+  } else {
+    content = (
+      <>
         <CampsiteDetail campsite={campsite} />
         <CommentsList campsiteId={campsiteId} />
-      </Row>
+      </>
+    );
+  }
+
+  return (
+    <Container>
+      {campsite && <SubHeader current={campsite.name} detail={true} />}
+      <Row>{content}</Row>
     </Container>
   );
 };
